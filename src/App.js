dc544b79ac21b7260a7d92607bdd513108b240c5
simoneraich <110484069+simoneraich@users.noreply.github.com>
2023-03-16 11:36:18 +0100

    added basic 'projects' structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './Hero.js';
 import About from './About.js';
+import Projects from './Projects.js';
 
 function App() {
   return (
     <div className="App">
     <Hero />
     <About />
+    <Projects />
     </div>
   );
 }
