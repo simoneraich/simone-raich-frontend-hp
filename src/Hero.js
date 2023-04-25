4666681ae445229999bb27adb3efc10565cdd12d
simoneraich <110484069+simoneraich@users.noreply.github.com>
2023-04-25 15:34:47 +0200

    minor design changes

--- src/Hero.js
+++ src/Hero.js
@@ -6,15 +6,15 @@ import portrait from "./portrait.png";
 export default function Hero() {
   return (
     <div className="hero">
-  <div class="hero-container" style={{ display: "flex" }}>
-    <div class="name" style={{ flex: 3 }}>
+  <div className="hero-container" style={{ display: "flex" }}>
+    <div className="name" style={{ flex: 3 }}>
       <h1>Simone Raich</h1>
       </div>
-    <div class="circle" style={{ flex: 6}}> 
+    <div className="circle" style={{ flex: 6}}> 
         <img src={portrait} alt="Portrait" style={{ position: "absolute", zIndex: 1}}/>
 </div>
     
-      <div class="social-icons" style={{ flex: 2 }}>
+      <div className="social-icons" style={{ flex: 2 }}>
         <img src="#" alt="social icon"/>
       </div>
     
